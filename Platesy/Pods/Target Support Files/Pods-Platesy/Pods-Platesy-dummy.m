d35060e2be274b24bd8a18d1c99605c8381a80b8
Christopher Kuhn <kuhncj@miamioh.edu>
2018-05-11 16:50:22 -0400

    + AWS Initial Support

---
+++ Platesy/Pods/Target Support Files/Pods-Platesy/Pods-Platesy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Platesy : NSObject
+@end
+@implementation PodsDummy_Pods_Platesy
+@end
---
+++ Platesy/Pods/Target Support Files/Pods-PlatesyUITests/Pods-PlatesyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlatesyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PlatesyUITests
+@end
